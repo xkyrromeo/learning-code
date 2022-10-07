a4132b6e490d09796456ebb7f3512e3909db02fe
xkyrromeo <xavierkrainbow@gmail.com>
2022-10-07 17:02:00 +0100

    Added function mathematical_calculations, which uses numpy and will be used to perform statistical analysis on price data.

--- stock_price.py
+++ stock_price.py
@@ -2,6 +2,7 @@ import json
 import requests
 import time
 import pandas as pd
+import numpy as np
 
 
 def get_price(ticker):
@@ -14,15 +15,26 @@ def get_price(ticker):
     close_list = response['chart']['result'][0]['indicators']['quote'][0]['close']
     time_list = response['chart']['result'][0]['timestamp']
     df_list = []
-    for i in range(len(time_list)):
+    for i in range(len(time_list)-1):
         if close_list[i] is None or open_list[i] is None:
             continue
         price_diff = close_list[i] - open_list[i]
+        price_av = (close_list[i] + open_list[i])/2
         formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time_list[i]))
-        result_dict = {'time': formatted_time, 'price difference': price_diff, 'close price': close_list[i], 'open price': open_list[i]}
+        result_dict = {'time': formatted_time, 'price difference': price_diff, 'close price': close_list[i], 'open price': open_list[i], 'average price': price_av}
         df_list.append(result_dict)
     df = pd.DataFrame(df_list)
-    df.set_index('time', inplace=True)
-    df.to_excel(f'{ticker}.xlsx')
-    return df
-get_price('GOOGLE')
+    df.set_index('price difference', inplace=True)
+    #df.to_excel(f'{ticker}.xlsx')
+    return mathematical_calculations(df)
+
+
+def mathematical_calculations(price_df):
+    print(price_df)
+    price_diff_array = price_df.index.to_numpy().astype(float)
+    print(price_diff_array)
+    print(np.mean(price_diff_array))
+    print(np.sort(price_diff_array))
+    print(np.std(price_diff_array))
+    print(np.percentile(price_diff_array, 97.5))
+get_price('AAPL')
